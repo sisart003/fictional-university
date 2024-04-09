e78226c816275714d27156ef7a9d1bdfbdee4a35
Chrishart <chrishart.estrada@gmail.com>
2024-04-09 20:24:46 +0800

    Static Converted

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+
+function university_files() {
+    wp_enqueue_script('main-university-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
+    wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+    wp_enqueue_style('font-awesome', '//cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css');
+    wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));
+    wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
+}
+
+add_action('wp_enqueue_scripts', 'university_files');
