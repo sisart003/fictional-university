d15333a953e3533d8cb4ca226ef28e6af8254ad2
Chrishart <chrishart.estrada@gmail.com>
2024-04-12 19:20:47 +0800

    only show upcoming events

--- functions.php
+++ functions.php
@@ -15,6 +15,24 @@ add_action('wp_enqueue_scripts', 'university_files');
 function university_features(){
 	add_theme_support('title-tag');	
 }
+
 add_action('after_setup_theme', 'university_features');
 
+function university_adjust_queries($query){
+    if(!is_admin() && is_post_type_archive('event') && $query->is_main_query()){
+        $today = date('Ymd');
+        $query->set('meta_key', 'event_date');
+        $query->set('orderby', 'meta_value_num');
+        $query->set('order', 'ASC');
+        $query->set('meta_query', array(
+            array(
+              'key' => 'event_date',
+              'compare' => '>=',
+              'value' => $today,
+              'type' => 'numeric'
+            )
+          ));
+    }
+}
 
+add_action('pre_get_posts', 'university_adjust_queries');
