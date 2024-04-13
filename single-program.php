c875ec2137155f736caceb1cf07b8c43cfd77597
Chrishart <chrishart.estrada@gmail.com>
2024-04-13 16:53:12 +0800

    Custom Image Cropping

--- single-program.php
+++ single-program.php
@@ -48,7 +48,7 @@
             $relatedProfessors->the_post(); ?>
             <li class="professor-card__list-item">
               <a class="professor-card" href="<?php the_permalink(); ?>">
-                <img src="<?php the_post_thumbnail_url(); ?>" class="professor-card__image" alt="">
+                <img src="<?php the_post_thumbnail_url('professorLandscape'); ?>" class="professor-card__image" alt="">
                 <span class="professor-card__name"><?php the_title(); ?></span>
               </a>
             </li>
