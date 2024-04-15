7dfad1bfa698674d8a13f8bc2cfe7acd787e919c
Chrishart <chrishart.estrada@gmail.com>
2024-04-16 03:44:04 +0800

    Its not working due to credential issues in google

--- functions.php
+++ functions.php
@@ -24,6 +24,10 @@ function university_features(){
 add_action('after_setup_theme', 'university_features');
 
 function university_adjust_queries($query){
+  if(!is_admin() && is_post_type_archive('campus') && is_main_query()){
+    $query->set('posts_per_page', -1);
+  }
+
     if(!is_admin() && is_post_type_archive('program') && is_main_query()){
       $query->set('orderby', 'title');
       $query->set('order', 'ASC');
