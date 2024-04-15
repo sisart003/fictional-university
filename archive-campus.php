38216648e3bc919a440af4fe167cc447a43ac464
Chrishart <chrishart.estrada@gmail.com>
2024-04-16 02:37:18 +0800

    connecting campus and program

--- archive-campus.php
+++ archive-campus.php
@@ -17,10 +17,13 @@
             <?php
                 while(have_posts()){
                 the_post();
-                $mapLocation = get_field('map_location'); ?>
-                <div data-lat="<?php echo $mapLocation['lat']; ?>" data-lng="<?php echo $mapLocation['lng']; ?>"></div>
+                // $mapLocation = get_field('map_location'); ?>
+                <!-- <div data-lat="<?php echo $mapLocation['lat']; ?>" data-lng="<?php echo $mapLocation['lng']; ?>">
+                    <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                    <?php echo $mapLocation['address']; ?>
+                </div> -->
+                <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
             <?php  }
-                echo paginate_links();
             ?>
         </div>
     </div>
