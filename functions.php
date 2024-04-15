151c5aac41b931b91be4d226a4c4e650465e18fe
Chrishart <chrishart.estrada@gmail.com>
2024-04-16 01:07:40 +0800

    add google map api key but commented (not working)

--- functions.php
+++ functions.php
@@ -1,6 +1,7 @@
 <?php
 
 function university_files() {
+    // wp_enqueue_script('googleMap', '//maps.googleapis.com/maps/api/js?key=' , NULL, '1.0', true);
     wp_enqueue_script('main-university-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
     wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
     wp_enqueue_style('font-awesome', '//cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css');
@@ -46,3 +47,10 @@ function university_adjust_queries($query){
 }
 
 add_action('pre_get_posts', 'university_adjust_queries');
+
+// function universityMapKey($api){
+//    $api['key'] = 'API KEY';
+//    return $api;
+// }
+
+// add_filter('acf/fields/google_map/api', 'universityMapKey');
