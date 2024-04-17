ba1c364438e26ab5d0d8af3a3ac810ec4d5e957e
Chrishart <chrishart.estrada@gmail.com>
2024-04-17 20:43:16 +0800

    Rest API added on search

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery'), 'version' => 'd91e2e7e2c585ff0b1df');
+<?php return array('dependencies' => array('jquery'), 'version' => 'd8750734999801f380db');
--- src/modules/Search.js
+++ src/modules/Search.js
@@ -7,10 +7,11 @@ class Search {
     this.openButton = $(".js-search-trigger")
     this.closeButton = $(".search-overlay__close")
     this.searchOverlay = $(".search-overlay")
+    this.searchField = $("#search-term")
     this.events()
-    this.isOverlayOpen = false;
-    this.isSpinnerVisible = false;
-    this.previousValue;
+    this.isOverlayOpen = false
+    this.isSpinnerVisible = false
+    this.previousValue
     this.typingTimer
   }
 
@@ -18,51 +19,58 @@ class Search {
   events() {
     this.openButton.on("click", this.openOverlay.bind(this))
     this.closeButton.on("click", this.closeOverlay.bind(this))
-    $(document).on("keyup", this.keyPressDispatcher.bind(this))
-    this.searchField.on("keydown", this.typingLogic.bind(this));
+    $(document).on("keydown", this.keyPressDispatcher.bind(this))
+    this.searchField.on("keyup", this.typingLogic.bind(this))
   }
 
   // 3. methods (function, action...)
-  typingLogic(){
-    if(this.searchField.val() != this.previousValue){
-      clearTimeout(this.typingTimer);
+  typingLogic() {
+    if (this.searchField.val() != this.previousValue) {
+      clearTimeout(this.typingTimer)
 
-      if(this.searchField.val()){
-        if(!this.isSpinnerVisible){
+      if (this.searchField.val()) {
+        if (!this.isSpinnerVisible) {
           this.resultsDiv.html('<div class="spinner-loader"></div>')
-          this.isSpinnerVisible = true;
+          this.isSpinnerVisible = true
         }
-        this.typingTimer = setTimeout(this.getResults.bind(this), 2000);
+        this.typingTimer = setTimeout(this.getResults.bind(this), 2000)
+      } else {
+        this.resultsDiv.html("")
+        this.isSpinnerVisible = false
       }
-      }else{
-        this.resultsDiv.html('');
-        this.isSpinnerVisible = false;
-      }
+    }
 
-      
-    
-    this.previousValue = this.searchField.val();
+    this.previousValue = this.searchField.val()
   }
 
-  getResults(){
-    this.resultsDiv.html("Imagine real serach results here...")
-    this.isSpinnerVisible = false;
+  getResults() {
+    $.getJSON("http://localhost/fictional-university/wp-json/wp/v2/posts?search=" + this.searchField.val(), function (posts) {
+      alert(posts[0].title.rendered)
+    })
   }
 
-  keyPressDispatcher(e){
-    if(e.keyCode == 27){
-      this.closeOverlay();
+  keyPressDispatcher(e) {
+    if (e.keyCode == 83 && !this.isOverlayOpen && !$("input, textarea").is(":focus")) {
+      this.openOverlay()
+    }
+
+    if (e.keyCode == 27 && this.isOverlayOpen) {
+      this.closeOverlay()
     }
   }
 
   openOverlay() {
     this.searchOverlay.addClass("search-overlay--active")
     $("body").addClass("body-no-scroll")
+    console.log("our open method just ran!")
+    this.isOverlayOpen = true
   }
 
   closeOverlay() {
     this.searchOverlay.removeClass("search-overlay--active")
     $("body").removeClass("body-no-scroll")
+    console.log("our close method just ran!")
+    this.isOverlayOpen = false
   }
 }
 
