fb2c01739915e059b45a9dc1f0a15d0787395b78
Chrishart <chrishart.estrada@gmail.com>
2024-04-18 19:10:45 +0800

    callback get_the_author()

--- functions.php
+++ functions.php
@@ -1,5 +1,13 @@
 <?php
 
+function university_custom_rest(){
+  register_rest_field('post', 'authorName', array(
+    'get_callback' => function() { return get_the_author(); }
+  ));
+}
+
+add_action('rest_api_init', 'university_custom_rest');
+
 function university_files() {
     // wp_enqueue_script('googleMap', '//maps.googleapis.com/maps/api/js?key=' , NULL, '1.0', true);
     wp_enqueue_script('main-university-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
