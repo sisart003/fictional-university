5f2a65c7655d4bef239e18fbfc8357f160cc702b
Chrishart <chrishart.estrada@gmail.com>
2024-04-18 19:11:17 +0800

    Added author name to post item

--- src/modules/Search.js
+++ src/modules/Search.js
@@ -47,13 +47,13 @@ class Search {
   getResults() {
     $.when(
       $.getJSON(universityData.root_url + "/wp-json/wp/v2/posts?search=" + this.searchField.val()),
-      $.getJSON(universityData.root_url + "/wp-json/wp/v2/pages?search=" + this.searchField.val(), f)
+      $.getJSON(universityData.root_url + "/wp-json/wp/v2/pages?search=" + this.searchField.val())
     ).then((posts, pages) => {
       var combinedResults = posts[0].concat(pages[0]);
         this.resultsDiv.html(`
           <h2 class="search-overlay__section-title">General Information</h2>
           ${combinedResults.length ? '<ul class="link-list min-list">' : '<p>No results found.</p>'}
-            ${combinedResults.map(item => `<li><a href="${item.link}">${item.title.rendered}</a></li>`).join('')}
+            ${combinedResults.map(item => `<li><a href="${item.link}">${item.title.rendered}</a> ${item.type == 'post' ? `by ${item.authorName}` : ''}</li>`).join('')}
           ${combinedResults.length ? '</ul>' : ''}
         `);
         this.isSpinnerVisible = false;
