fe0ea96d008ec927401d692934e5abef18ce66b6
Chrishart <chrishart.estrada@gmail.com>
2024-04-18 19:42:37 +0800

    custom post added

--- inc/search-route.php
+++ inc/search-route.php
@@ -8,7 +8,21 @@ function universityRegisterSearch(){
 }
 
 function universitySearchResults(){
-    return 'Congrats';
+    $professors = new WP_Query(array(
+        'post_type' => 'professor'
+    ));
+
+    $professorResults = array();
+
+    while($professors->have_posts()){
+        $professors->the_post();
+        array_push($professorResults, array(
+            'title' => get_the_title(),
+            'url' => get_the_permalink()
+        ));
+    }
+
+    return $professorResults;
 }
 
 add_action('rest_api_init', 'universityRegisterSearch');
