22f4468cc8f2022ad2f853d069a958372135fe1c
Chrishart <chrishart.estrada@gmail.com>
2024-04-18 19:48:18 +0800

    rest api search term added

--- inc/search-route.php
+++ inc/search-route.php
@@ -9,7 +9,8 @@ function universityRegisterSearch(){
 
 function universitySearchResults(){
     $professors = new WP_Query(array(
-        'post_type' => 'professor'
+        'post_type' => 'professor',
+        's' => sanitize_text_field($data['term'])
     ));
 
     $professorResults = array();
