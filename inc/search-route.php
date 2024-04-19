014e62a8c777ff4f7edaff75c1ef7a0ff625fdc6
Chrishart <chrishart.estrada@gmail.com>
2024-04-20 07:59:23 +0800

    Search Dynamic

--- inc/search-route.php
+++ inc/search-route.php
@@ -44,7 +44,8 @@ function universitySearchResults(){
         if(get_post_type() == 'program'){
             array_push($results['programs'], array(
                 'title' => get_the_title(),
-                'permalink' => get_the_permalink()
+                'permalink' => get_the_permalink(),
+                'id' => get_the_id()
             ));
         }
 
@@ -76,29 +77,35 @@ function universitySearchResults(){
         
     }
 
-    $programRelationshipQuery = new WP_Query(array(
-        'post_type' => 'professor',
-        'meta_query' => array(
-            array(
-                'key'=> 'related_programs',
-                'compare' => 'LIKE',
-                'value' => '"97"'
-            )
-        )
-    ));
+    $programsMetaQuery = array('relation' => 'OR');
 
-    while($programRelationshipQuery->have_posts()){
-        $programRelationshipQuery->the_post();
-        if(get_post_type() == 'professor'){
-            array_push($results['professors'], array(
-                'title' => get_the_title(),
-                'permalink' => get_the_permalink(),
-                'image' => get_the_post_thumbnail_url(0, 'professorLandscape')
-            ));
-        }
+    foreach($results['programs'] as $item){
+        array_push($programsMetaQuery, array(
+            'key'=> 'related_programs',
+            'compare' => 'LIKE',
+            'value' => '"'. $item['id'] .'"'
+        ),);
     }
 
-    $results['professors'] = array_values(array_unique($results['professors'], SORT_REGULAR));
+    if($results['programs']){
+        $programRelationshipQuery = new WP_Query(array(
+            'post_type' => 'professor',
+            'meta_query' => $programsMetaQuery
+        ));
+    
+        while($programRelationshipQuery->have_posts()){
+            $programRelationshipQuery->the_post();
+            if(get_post_type() == 'professor'){
+                array_push($results['professors'], array(
+                    'title' => get_the_title(),
+                    'permalink' => get_the_permalink(),
+                    'image' => get_the_post_thumbnail_url(0, 'professorLandscape')
+                ));
+            }
+        }
+    
+        $results['professors'] = array_values(array_unique($results['professors'], SORT_REGULAR));
+    }
 
     return $results;
 }
