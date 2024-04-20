adc4f3544987de601eeefe1a4b0e363022eb9c1c
Chrishart <chrishart.estrada@gmail.com>
2024-04-20 19:59:34 +0800

    search page template form added

--- page-search.php
+++ page-search.php
@@ -57,20 +57,7 @@
             </ul>
           </div>
       <?php  }
-      ?>
-
-      <div class="generic-content">
-		<form method="get" action="<?php echo esc_url(site_url('/')); ?>">
-            <label class="headline headline--medium" for="s">Perform a Search:</label>
-            <div class="search-form-row">
-                <input placeholder="What are you looking for?" class="s" id="s" type="search" name="s">
-                <input class="search-submit" type="submit" value="Search">
-            </div>
-        </form>
-	  </div>
-    </div>
-	
-  <?php }
+      get_search_form(); }
 
   get_footer();
 
