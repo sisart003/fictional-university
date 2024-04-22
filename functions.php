d934d6576dc0a6443bc558836451ec21054fb92e
Chrishart <chrishart.estrada@gmail.com>
2024-04-22 21:13:59 +0800

    notes basic security improved

--- functions.php
+++ functions.php
@@ -117,3 +117,21 @@ add_filter('login_headertitle', 'ourLoginTitle');
 function ourLoginTitle(){
   return get_bloginfo('name');
 }
+
+// Force note posts to be private
+
+function makeNotePrivate($data){
+
+  if($data['post_type'] == 'note'){
+    $data['post_content'] = sanitize_textarea_field($data['post_content']);
+    $data['post_title'] = sanitize_text_field($data['post_title']);
+  }
+
+  if($data['post_type'] == 'note' && $data['post_status'] != 'trash'){
+    $data['post_status'] = "private";
+  }
+  
+  return $data;
+}
+
+add_filter('wp_insert_post_data', 'makeNotePrivate');
