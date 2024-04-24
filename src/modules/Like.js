8aeda8e2bff1b5787838c51ec27983eaec204226
Chrishart <chrishart.estrada@gmail.com>
2024-04-24 10:38:16 +0800

    Professor ID continues

--- src/modules/Like.js
+++ src/modules/Like.js
@@ -1,51 +1,54 @@
-import $ from 'jquery';
+import $ from "jquery"
 
-class Like{
-    constructor(){
-        this.events();
+class Like {
+  constructor() {
+    this.events()
+  }
+
+  events() {
+    $(".like-box").on("click", this.ourClickDispatcher.bind(this))
+  }
+
+  // methods
+  ourClickDispatcher(e) {
+    var currentLikeBox = $(e.target).closest(".like-box")
+
+    if (currentLikeBox.data("exists") == "yes") {
+      this.deleteLike(currentLikeBox)
+    } else {
+      this.createLike(currentLikeBox)
     }
+  }
 
-    events(){
-        $(".like-box").on("click", this.ourClickDispatcher.bind(this));
-    }
+  createLike(currentLikeBox) {
+    $.ajax({
+      beforeSend: xhr => {
+        xhr.setRequestHeader("X-WP-Nonce", universityData.nonce)
+      },
+      url: universityData.root_url + "/wp-json/university/v1/manageLike",
+      type: "POST",
+      data: { "professorId": currentLikeBox.data("professor") },
+      success: response => {
+        console.log(response)
+      },
+      error: response => {
+        console.log(response)
+      }
+    })
+  }
 
-    // methods
-    ourClickDispatcher(){
-        var currentLikeBox = $(e.target).closest(".like-box");
-
-        if(currentLikeBox.data('exists') == 'yes'){
-            this.deleteLike(currentLikeBox);
-        }else{
-            this.createLike(currentLikeBox);
-        }
-    }
-
-    createLike(currentLikeBox){
-        $.ajax({
-            url: universityData.root_url + '/wp-json/university/v1/manageLike',
-            type: 'POST',
-            data: {'professorId' : currentLikeBox.data('professor')},
-            success: (response) => {
-                console.log(response);
-            },
-            error: (response) => {
-                console.log(response)
-            }
-        })
-    }
-
-    deleteLike(currentLikeBox){
-        $.ajax({
-            url: universityData.root_url + '/wp-json/university/v1/manageLike',
-            type: 'DELETE',
-            success: (response) => {
-                console.log(response);
-            },
-            error: (response) => {
-                console.log(response)
-            }
-        })
-    }
+  deleteLike() {
+    $.ajax({
+      url: universityData.root_url + "/wp-json/university/v1/manageLike",
+      type: "DELETE",
+      success: response => {
+        console.log(response)
+      },
+      error: response => {
+        console.log(response)
+      }
+    })
+  }
 }
 
-export default Like;
+export default Like
