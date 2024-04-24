3a2fc1fe9a8b7385abe44550ff5308d3a8438014
Chrishart <chrishart.estrada@gmail.com>
2024-04-24 10:38:53 +0800

    get the Professor ID Value continues

--- inc/like-route.php
+++ inc/like-route.php
@@ -13,19 +13,43 @@ function universityLikeRoutes(){
 }
 
 function createLike($data){
-    sanitize_text_field($data['professorId']);
-    wp_insert_post(array(
-        'post_type' => 'like',
-        'post_status' => 'publish',
-        'post_title' => 'Our PHP Create Post Test',
-        'meta_input' => array(
-            'liked_professor_id' => 12345
-        )
-    ));
+
+    if(is_user_logged_in()){
+        $professor = sanitize_text_field($data['professorId']);
+        
+        $existQuery = new WP_Query(array(
+            'author' => get_current_user_id(),
+            'post_type' => 'like',
+            'meta_query' => array(
+              array(
+                'key' => 'liked_professor_id',
+                'compare' => '=',
+                'value' => $professor
+              )
+            )
+          ));
+        
+        if($existQuery->found_posts == 0 && get_post_type($professor) == 'professor'){
+            return wp_insert_post(array(
+                'post_type' => 'like',
+                'post_status' => 'publish',
+                'post_title' => 'Our PHP Create Post Test',
+                'meta_input' => array(
+                    'liked_professor_id' => $professor
+                )
+            ));   
+        }else{
+            die('Invalid Professir ID');
+        }
+    }else{
+        die("Only Logged in users can create a like.");
+    }
+
+    
 }
 
 function deleteLike(){
     return 'Thanks for trying to delete a like.';
 }
 
-add_action('rest_api_init');
+add_action('rest_api_init', 'universityLikeRoutes');
